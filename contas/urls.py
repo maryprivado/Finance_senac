267382e0c7c6525cea19f7f556c9a5ac199e2077
Rosimary <Privado_mary@hotmail.com>
2023-08-23 20:13:10 -0300

    contas

--- contas/urls.py
+++ contas/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
         path('definir_contas/', views.definir_contas, name="definir_contas"),
         path('ver_contas/', views.ver_contas, name="ver_contas"),
-        
 ]
